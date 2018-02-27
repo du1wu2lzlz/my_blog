05ae0cead7234d396786689e89194cc0a89f947a
luke <625376618@qq.com>
2018-02-27 11:35:13 +0800

    Update 将url参数解析为一个对象.js

--- Algorithm/将url参数解析为一个对象.js
+++ Algorithm/将url参数解析为一个对象.js
@@ -1,29 +1,25 @@
+// 请编写一个JavaScript函数 parseQueryString，它的用途是把URL参数解析为一个对象
 
- var url = "http://witmax.cn/index.php?key0=0&key1=1&key2=2";
-        function parseQueryString(url){
-             var params = {};
-             var arr = url.split("?");
-             if(arr.length <= 1){
-                    return params;
-             }
-             arr = arr[1].split("&");
-             for(var i =0;i<arr.length ;i++){
-                 var a = arr[i].split("=");
-                 params[a[0]] = a[1];
-             }
-             return params;
-             /*
-                hash        设置或返回从井号 (#) 开始的 URL（锚）。
-                host        设置或返回主机名和当前 URL 的端口号。
-                hostname    设置或返回当前 URL 的主机名。
-                href        设置或返回完整的 URL。
-                pathname    设置或返回当前 URL 的路径部分。
-                port        设置或返回当前 URL 的端口号。
-                protocol    设置或返回当前 URL 的协议。
-                search      设置或返回从问号 (?) 开始的 URL（查询部分）
-             */
-        }
-        console.info(window.location.search);//?key0=0&key1=1&key2=2 
-        console.info("解析http://witmax.cn/index.php?key0=0&key1=1&key2=2");
-        console.info(parseQueryString(url));
-        console.info("源码parseQueryString");
+// 如：var url = ”http://www.taobao.com/index.php?key0=0&key1=1& key2=2”
+// var obj = new parseQueryString(url);
+// alert(obj.key0) // 输出0
+
+
+
+var url = "http://www.taobao.com/index.php?key0=0&key1=1&key2=2";
+
+ function parseQueryString(url){
+     var str = url.split("?")[1],    //通过?得到一个数组,取?后面的参数
+         items = str.split("&");    //分割成数组
+     var arr,name,value;
+ 
+     for(var i=0; i<items.length; i++){
+         arr = items[i].split("=");    //["key0", "0"]
+         name = arr[0];
+         value = arr[1];
+         this[name] = value;
+     }
+ }
+ 
+ var obj = new parseQueryString(url);
+ alert(obj.key2)
